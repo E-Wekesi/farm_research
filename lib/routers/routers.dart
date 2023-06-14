95adc048e8e2d73a08fbe212d23447d56727ffa1
Anida Kimtai <61341735+anida15@users.noreply.github.com>
2023-06-14 17:07:36 +0300

    add name

--- lib/routers/routers.dart
+++ lib/routers/routers.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "start",
+      title: "start up",
       initialRoute: '/',
       routes: {
         '/': (context) =>    const Home(),
